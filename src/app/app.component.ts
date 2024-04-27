571014bf9530d22d256f370bdbc0adbe72187709
Dimitrios Milathianakis <dimilathian@gmail.com>
2024-04-27 12:37:53 +0300

    variable types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,37 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular';
+  title: string = 'angular';
+
+  version: number = 1;
+  isNew: boolean = true;
+  boxesNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  boxesStrings: string[] = ['cat', 'dog', 'elephant'];
+  boxesBoolean: boolean[] = [true, false, true];
+  boxes: (string | number)[] = [1, 'cat'];
+  boxesMix: (string | number | boolean)[] = [1, 'cat', true];
+
+  x: object = {
+    a: 'cat',
+    b: 'dog',
+    c: 'elephant',
+    d: 1,
+    e: 2,
+    f: true,
+    g: false,
+    a1: 1,
+    a2: 'oti thelw',
+    a3: 'oti thelw'
+  };
+
+  constructor() {
+    this.print();
+  }
+
+  print() {
+    console.log(this.x);
+    console.log(this.boxesMix);
+  }
 
   receiveContentFromChild(content: any) {
     console.log('Content received from child:', content);
