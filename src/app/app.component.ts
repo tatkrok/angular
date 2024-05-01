4fba455adb168e77306c07642f7ce3f3e456a1ba
Dimitrios Milathianakis <dimilathian@gmail.com>
2024-05-01 20:37:21 +0300

    move box data to typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,11 @@ export class AppComponent {
 
   version: number = 1;
   isNew: boolean = true;
-  boxesNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  boxesStrings: string[] = ['cat', 'dog', 'elephant'];
-  boxesBoolean: boolean[] = [true, false, true];
-  boxes: (string | number)[] = [1, 'cat'];
-  boxesMix: (string | number | boolean)[] = [1, 'cat', true];
-
-  FirstVersion: number = 1;
-  isFirst: boolean = true;
-  circlesNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  circlesStrings: string[] = ['car', 'aeroplane', 'boat'];
-  circlesBoolean: boolean[] = [true, false, true];
-  circles: (string | number)[] = [1, 'car'];
-  circlesMix: (string | number | boolean)[] = [1, 'car', true];
-
+  // boxesNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // boxesStrings: string[] = ['cat', 'dog', 'elephant'];
+  // boxesBoolean: boolean[] = [true, false, true];
+  // boxes: (string | number)[] = [1, 'cat'];
+  // boxesMix: (string | number | boolean)[] = [1, 'cat', true];
   x: any = {
     a: 'cat',
     b: 'dog',
@@ -37,36 +28,33 @@ export class AppComponent {
     a3: 'oti thelw',
   };
 
-  y: any = {
-    a: 'car',
-    b: 'aeroplane',
-    c: 'boat',
-    d: 1,
-    e: 2,
-    f: true,
-    g: false,
-    a1: 1,
-    a2: 'trip',
-    a3: 'trip',
-  };
+  numbers2: number[] = [1, 2, 3, 4, 5, 6];
+
+  boxes: any[] = [
+    {
+      content: 'Box 1 content',
+      subtitle: 'Box 1 subtitle',
+    },
+    {
+      content: 'Box 2 content',
+      subtitle: 'Box 2 subtitle',
+    },
+    {
+      content: 'Box 3 content',
+      subtitle: 'Box 3 subtitle',
+    },
+  ];
 
   constructor() {
+    console.log(this.numbers2[1])
+    console.log(this.boxes)               // array
+    console.log(this.boxes[0])            // object
+    console.log(this.boxes[0].content)    // string
     this.print();
-    this.printAgain();
   }
 
   print() {
     console.log(this.x);
-    console.log(this.boxesMix);
-    console.log(this.y['c']);
-  }
-
-  printAgain() {
-    console.log(this.y);
-    console.log(this.circlesMix);
-    console.log(this.y['a']);
-    console.log(this.x['c']);
-    console.log(this.circlesNumbers);
   }
 
   receiveContentFromChild(content: any) {
