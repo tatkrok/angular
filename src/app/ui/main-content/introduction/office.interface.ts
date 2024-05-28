5d8ed154ca1799fc5f324748456f9b1f45fd2fa5
Dimitrios Milathianakis <dimilathian@gmail.com>
2024-05-28 22:37:17 +0300

    task

---
+++ src/app/ui/main-content/introduction/office.interface.ts
@@ -0,0 +1,22 @@
+const mobile: any = {
+  brand: 'Apple',
+  screen: '6.1',
+  ram: '4GB',
+  cameraFront: '16mp',
+  cameraBack: '32mp',
+  cost: 800
+}
+
+const office: any = {
+  monitors: [
+    { size: '24px', brand: 'Dell', microphoneAvailable: true },
+    { size: '32px', brand: 'Samsung', microphoneAvailable: false }
+  ],
+  mouse: { brand: 'Logitech', isAnatomic: true },
+  keyboard: { brand: 'Logitech', isMechanic: false },
+  hasCamera: true,
+}
+
+// Thelw stis parapanw 2 metavlites na pas kai na moy ftiaxeis Interface.
+// An se voleuei na tis exeis se xexwristo arxeio ftiaxe allo ena kai metefere ekei to mobile. Alliws mporeis edw kai gia ta 2.
+// Good luck :P
