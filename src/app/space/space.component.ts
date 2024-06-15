2d73bedc71e97f6c7098175a6e95c38dc870a352
Dimitrios Milathianakis <dimilathian@gmail.com>
2024-06-15 11:41:12 +0300

    ngFor planets

--- src/app/space/space.component.ts
+++ src/app/space/space.component.ts
@@ -6,14 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './space.component.css',
 })
 export class SpaceComponent {
-  planetMars: string = 'Mars';
-  planetEarth: string = 'Earth';
-  planetNeptune: string = 'Neptune';
-  planetMercury: string = 'Mercury';
-  planetVenus: string = 'Venus';
-  planetJupiter: string = 'Jupiter';
-  planetPluto: string = 'Pluto';
-  planetEris: string = 'Eris';
+  planets: string[] = [
+    'Mars',
+    'Earth',
+    'Neptune',
+    'Mercury',
+    'Venus',
+    'Jupiter',
+    'Pluto',
+    'Eris'
+  ];
 
   meteorite1: number = 1;
   meteorite2: number = 2;
